8623915618c184200593b53553aea6cfe89d61da
Dawid Kolodziej <dawid_kolodziej@o2.pl>
2020-11-30 18:52:54 +0100

    complex equasion

--- src/main.kt
+++ src/main.kt
@@ -1,53 +1,76 @@
+import java.lang.Math.abs
+import java.lang.Math.exp
 import java.util.Scanner
 
 fun main() {
 
 
-        val reader = Scanner(System.`in`)
-        print("Enter a whole number: ")
+    val reader = Scanner(System.`in`)
+    print("Enter a whole number: ")
 
-        try {
-            var x = reader.nextInt()
+    try {
+        var x = reader.nextInt()
 
 
-            val result = when (x) {
-                0 -> {
-                    println("x is equal to zero")
+        val result = when (x) {
+            0 -> {
+                println("x is equal to zero")
 
-                }
-                1, 2, 3, 4, 5, 6 -> {
-                    println("x belongs to the set of integers {1 to 6} ")
-
-                }
-                in 8..20 -> {
-                    println("x is within the range of 8..20")
-
-                }
-                else -> {
-                    println("is greater than 21")
-
-                }
             }
+            1, 2, 3, 4, 5, 6 -> {
+                println("x belongs to the set of integers {1 to 6} ")
 
+            }
+            in 8..20 -> {
+                println("x is within the range of 8..20")
 
-            println("result ${result}")
-        }catch (e: Exception){
-            println("It is not a whole number")
-            main()
+            }
+            else -> {
+                println("is greater than 21")
+
+            }
         }
 
-                val readerA = Scanner(System.`in`)
-                println("Enter a whole number: ")
-                try {
-                    val y = readerA.nextInt()
-                    var q = when(y){
-                        0 -> 5
-                        in 123..500->6
-                        else->777
-                    }
-                    println("Number q is ${q}")
-                }catch (e: Exception){
-                    println("You haven't entered an integer")
-                    main()
-                }
+
+        println("result ${result}")
+    } catch (e: Exception) {
+        println("It is not a whole number")
+        main()
     }
+
+    val readerA = Scanner(System.`in`)
+    println("Enter a whole number: ")
+    try {
+        val y = readerA.nextInt()
+        var q = when (y) {
+            0 -> 5
+            in 123..500 -> 6
+            else -> 777
+        }
+        println("Number q is ${q}")
+
+    } catch (e: Exception) {
+        println("You haven't entered an integer")
+        main()
+    }
+
+
+    println("And now, we will try something more advanced")
+
+
+    fun complexEquasion(){
+        val equa: (Double)->Double = {x:Double -> x * (Math.pow(Math.E,(-1* (Math.PI/2))))}
+
+        println((Math.pow(Math.E,(-1* (Math.PI/2)))))
+
+
+        var x = Math.random()*10
+
+        val res = equa(x)
+        println("The value of $x * i ^ i is ${res}")
+
+    }
+
+    complexEquasion()
+
+}
