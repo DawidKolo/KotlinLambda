70d2e104f60f14b245249ef849990f8cf9d480e6
Dawid Kolodziej <dawid_kolodziej@o2.pl>
2020-12-01 21:15:30 +0100

    firstNotNull

--- src/main.kt
+++ src/main.kt
@@ -21,6 +21,7 @@ fun main() {
         main()
     }
     numberToString()
+    notNull()
 }
 
 // Function defined below
@@ -156,10 +157,40 @@ fun numberToString() {
         }
 
         println(f(a, b, c))
-    }catch (e: Exception){
+    } catch (e: Exception) {
         numberToString()
     }
+
 }
+
+
+
+fun notNull(){
+    try {
+        println("Enter 1 to start notNull part of the program")
+        val t = Scanner(System.`in`)
+        val h = t.nextInt()
+
+        if (h == 1) {
+
+            val table: Array<Int?> = arrayOf(1, 2, null, 3, 4, 5, null, null)
+            val tableA = table.filterNotNull()
+
+            println("This is a original table: "+table.asList())
+            println("This is a filtered one: "+tableA)
+
+
+        } else {
+            exitProcess(0)
+        }
+    } catch (e: Exception) {
+        println("Just numbers please")
+        notNull()
+    }
+
+}
+
+
 
 
 
