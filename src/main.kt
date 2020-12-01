99ba208a16b592e384aa45cba3216267348d1ef0
Dawid Kolodziej <dawid_kolodziej@o2.pl>
2020-12-01 18:37:35 +0100

    last tryCatch

--- src/main.kt
+++ src/main.kt
@@ -139,23 +139,26 @@ fun mathDouble(i: Int, j: Int, f: (Int, Int) -> Double): Double {
 }
 
 fun numberToString() {
+    try {
+        println("Please enter an your name")
+        val name = Scanner(System.`in`)
+        val a = name.next()
 
-    println("Please enter an your name")
-    val name = Scanner(System.`in`)
-    val a = name.next()
+        println("Please enter an integer")
+        val b = name.nextInt()
 
-    println("Please enter an integer")
-    val b = name.nextInt()
+        println("Please enter another number")
+        val c = name.nextDouble()
 
-    println("Please enter another number")
-    val c = name.nextDouble()
+        val f: (String, Int, Double) -> String = { a: String, b: Int, c: Double ->
+            println("Hello " + a + "! \n1There is " + b * c + " PLN on your account")
+            "$a $b $c"
+        }
 
-    val f: (String, Int, Double) -> String = { a: String, b: Int, c: Double ->
-        println("Hello " + a + "! \n1There is " + b * c + " PLN on your account")
-        "$a $b $c"
+        println(f(a, b, c))
+    }catch (e: Exception){
+        numberToString()
     }
-
-    println(f(a, b, c))
 }
 
 
